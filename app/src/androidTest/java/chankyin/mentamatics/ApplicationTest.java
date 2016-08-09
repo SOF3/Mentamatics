057770c304d2e7d09a02f77f74994fdb1b1536f9
chankyin <chankyin1970@gmail.com>
2016-08-09 16:19:05 +0800

    Cleaned config files

--- app/src/androidTest/java/chankyin/mentamatics/ApplicationTest.java
+++ app/src/androidTest/java/chankyin/mentamatics/ApplicationTest.java
@@ -2,9 +2,6 @@ package chankyin.mentamatics;
 
 // import android.test.ApplicationTestCase;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
 public class ApplicationTest{ // extends ApplicationTestCase{
 	public ApplicationTest(){
 //		super(Main.class);
